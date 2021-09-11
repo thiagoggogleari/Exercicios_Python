2a6ea894f4c75925c1c86d85eece4a6fb0f392fc
Thiago <thiagoggogleari@gmail.com>
2021-09-11 16:37:00 -0300

    Criado programa

---
+++ Estrutura Sequencial/5 Converte m para cm.py
@@ -0,0 +1,5 @@
+# Faça um Programa que converta metros para centímetros.
+
+medida = float(input('Insira a medida em metros: '))
+
+print('%.2f metros é equivalente a %.2f centímetros.'%(medida,medida*100))
