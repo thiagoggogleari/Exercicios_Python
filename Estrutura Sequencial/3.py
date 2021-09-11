60f313521a577626fed12e42980aada1906052df
Thiago <thiagoggogleari@gmail.com>
2021-09-11 06:16:03 -0300

    Commit através do terminal

---
+++ Estrutura Sequencial/3.py
@@ -0,0 +1,9 @@
+# Faça um Programa que peça dois números e imprima a soma
+
+num1 = float(input('Insira um número: '))
+num2 = float(input('Inrira outro número: '))
+
+res = num1 + num2
+
+print('A soma de %i + %i é : %i'%(num1,num2,res))
+
