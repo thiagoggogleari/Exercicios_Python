ecf2efbc6307635db77d6b172ac31d9352faa165
Thiago <thiagoggogleari@gmail.com>
2021-09-12 07:11:01 -0300

    Padronizado nomes dos exercícios

---
+++ Estrutura Sequencial/Exercício 6.py
@@ -0,0 +1,8 @@
+# Faça um Programa que peça o raio de um círculo, calcule e mostre sua área.
+
+from math import pi
+
+raio = float(input('Qual o raio do círculo (cm) ? '))
+
+print('A área do círculo é de %.2f cm'%(pi* raio**2))
+
