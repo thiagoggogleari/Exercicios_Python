dfd9bc7c2c1b673f0a0505c16985997ad8051d29
Thiago <thiagoggogleari@gmail.com>
2021-09-12 07:24:39 -0300

    Criado programa

---
+++ Estrutura Sequencial/Exercício 7.py
@@ -0,0 +1,9 @@
+# Faça um Programa que calcule a área de um quadrado, em seguida mostre o dobro desta área para o usuário.
+
+medida = float(input('Insira a medida de um dos lados do quadrado (cm): '))
+
+
+# A área do quadrado é o produto entre os dois lados.
+print('A área do quadrado é de %.2fcm²'%(medida * 2))
+
+print('O dobro desta área apresentada é de %.2fcm²'%(medida * 4))
