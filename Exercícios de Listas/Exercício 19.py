45a3d63cf9a2f30df32c6d33cddb12349d52abbd
Thiago Ogleari <50802777+thiagoggogleari@users.noreply.github.com>
2022-02-01 20:27:29 -0300

    Create Exercício 19.py
    Estou trabalhando neste código. Exercício atual, ainda não está finalizado.

---
+++ Exercícios de Listas/Exercício 19.py
@@ -0,0 +1,81 @@
+#19
+# Uma empresa de pesquisas precisa tabular os resultados da seguinte enquete feita a um grande quantidade de organizações:
+# "Qual o melhor Sistema Operacional para uso em servidores?"
+
+# As possíveis respostas são:
+
+# 1- Windows Server
+# 2- Unix
+# 3- Linux
+# 4- Netware
+# 5- Mac OS
+# 6- Outro
+# Você foi contratado para desenvolver um programa que leia o resultado da enquete e informe ao final o resultado da mesma. O programa deverá ler os valores até ser informado o valor 0, que encerra a entrada dos dados. Não deverão ser aceitos valores além dos válidos para o programa (0 a 6). Os valores referentes a cada uma das opções devem ser armazenados num vetor. Após os dados terem sido completamente informados, o programa deverá calcular a percentual de cada um dos concorrentes e informar o vencedor da enquete. O formato da saída foi dado pela empresa, e é o seguinte:
+# Sistema Operacional     Votos   %
+# -------------------     -----   ---
+# Windows Server           1500   17%
+# Unix                     3500   40%
+# Linux                    3000   34%
+# Netware                   500    5%
+# Mac OS                    150    2%
+# Outro                     150    2%
+# -------------------     -----
+# Total                    8800
+
+# O Sistema Operacional mais votado foi o Unix, com 3500 votos, correspondendo a 40% dos votos.
+
+votos = []
+
+while True: 
+  entrada = int(input('Qual o melhor sistema operacional para uso em servidores? '))
+  if entrada == 0:
+      break
+  if entrada in (1,2,3,4,5,6):
+    votos.append(entrada)
+  else:
+    print('Entrada Inválida')
+
+
+print('\nForam computados {} votos.'.format(len(votos)))
+print('Os votos registrados foram: ')
+print(votos)
+
+maisVotado = 0
+qntdVotosMaisVotado = 0
+totalVotos = [] # Contagem total de votos
+
+for i in range(6): # Quantidade de sistemas operacionais disponíveis
+  somatoriaVotos = votos.count(i+1) # Conta o voto e adiciona o resultado a lista totalVotos
+  totalVotos.append(somatoriaVotos)
+  
+  if somatoriaVotos > qntdVotosMaisVotado:  # Verifica qual foi o mais votado
+    maisVotado = i + 1 
+    qntdVotosMaisVotado = somatoriaVotos
+  
+# numSistema = maisVotado
+
+listaSistemas = ['Windows Server','Unix','Linux','Netware','Mac OS','Outro']
+sistema = listaSistemas[maisVotado -1]
+ 
+print('\nResumo dos votos')
+print(totalVotos)
+
+espacos1 = 29
+espacos2 = 8
+
+# porcentagem = 17  #alterar
+# porcentagem1 = 3  # 
+
+print('\nSistema Operacional     Votos      %')
+print('-------------------     -----    -----')
+
+for i in range(len(listaSistemas)):
+  porcentagem = round((totalVotos[i] * 100) / len(votos),1)
+  print('{}{}{}{}{}%'.format(listaSistemas[i],' '*(espacos1 - len(listaSistemas[i]) - len(str(totalVotos[i]))),totalVotos[i],' '*(espacos2 - len(str(porcentagem))),porcentagem))
+
+
+
+
+print('\nO sistema operacional mais votado foi o {}, com {} votos, correspondendo a {:.1f}% dos votos'.format(sistema,qntdVotosMaisVotado,(qntdVotosMaisVotado * 100)/len(votos)))
+
+  
