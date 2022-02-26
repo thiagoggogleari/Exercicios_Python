ccdaa7af5e5c426974a2d24c9c1e2a723bb5ab22
Thiago Ogleari <thiagoggogleari@gmail.com>
2022-02-26 18:36:56 -0300

    Update Exercício 09.py

--- Exercícios de Funções/Exercício 09.py
+++ Exercícios de Funções/Exercício 09.py
@@ -1,4 +1,6 @@
-def reverso(entrada):
+# Reverso do número. Faça uma função que retorne o reverso de um número inteiro informado. 
+# Por exemplo: 127 -> 721.def reverso(entrada):
+
   texto = str(entrada)
   numeroReverso = ''
 
