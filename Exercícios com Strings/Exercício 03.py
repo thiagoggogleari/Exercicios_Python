14b11b0093ce89a0059871be137383196e780e13
Thiago Ogleari <thiagoggogleari@gmail.com>
2022-06-28 06:31:02 -0300

    Create Exercício 03.py

---
+++ Exercícios com Strings/Exercício 03.py
@@ -0,0 +1,13 @@
+#3 - Nome na vertical. Faça um programa que solicite o nome do usuário e imprima-o na vertical.
+
+#F
+#U
+#L
+#A
+#N
+#O
+
+nome = input("Insira o nome: ")
+
+for i in range(len(nome)):
+  print(nome[i])
